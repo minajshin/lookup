660d6fa8b30c2f7e76ea64fead45e5623ad54bd5
Mina Shin <me@minashin.net>
2020-07-19 16:22:23 -0700

    Create index page

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lookup</title>
+</head>
+<body>
+    <div class="main">
+        <div class="logo-wrapper">
+            <img src="assets/images/logo.png" alt="Lookup_Logo">
+        </div>
+
+        <div class="search-wrapper">
+            <input class="search-box" type="text" name="q">
+            <input class="serach-button" type="submit" value="Search">
+        </div>
+    </div>
+</body>
+</html>
